05d78747c45eb8e1d2bbd3aa0cc9ae2ea0747efa
jdgragon96 <tjdwodyd7712@gmail.com>
2025-01-21 21:16:37 +0900

    remove vs

--- NetworkCS/obj/Debug/net9.0-windows/NetworkCS.AssemblyInfo.cs
+++ NetworkCS/obj/Debug/net9.0-windows/NetworkCS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetworkCS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5874671338b859be3b3494c7321be01c5a59219")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetworkCS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetworkCS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
